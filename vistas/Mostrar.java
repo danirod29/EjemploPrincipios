8edb46760517f440ded9a81a6dd03e33a941b67b
danirod29 <51457784+danirod29@users.noreply.github.com>
2019-08-23 11:48:04 -0500

    Add files via upload

---
+++ vistas/Mostrar.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vistas;
+
+import Logica.Circulo;
+import Logica.Cuadrado;
+import Logica.FiguraGeometrica;
+import Logica.Rectangulo;
+import java.util.Scanner;
+
+public class Mostrar {
+    
+    
+    public static void main(String[] args) {
+    
+        VentanaPrincipal ven=new VentanaPrincipal();
+        ven.setVisible(true);
+    }  
+      
+    
+    
+}
